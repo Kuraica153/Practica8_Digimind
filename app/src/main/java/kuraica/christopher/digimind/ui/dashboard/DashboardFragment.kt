5b26d53ea0685cbc3244b62d400605f243ec1e63
Christopher Kuraica <christopherkuraica@gmail.com>
2023-05-04 16:43:53 -0700

    Updated Digimind app: Now you can add your own reminders to firebase

--- app/src/main/java/kuraica/christopher/digimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/kuraica/christopher/digimind/ui/dashboard/DashboardFragment.kt
@@ -11,6 +11,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kuraica.christopher.digimind.Reminder
 import kuraica.christopher.digimind.databinding.FragmentDashboardBinding
 import kuraica.christopher.digimind.ui.home.HomeFragment
@@ -20,6 +22,7 @@ import java.util.*
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
+    val db = Firebase.firestore
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -66,27 +69,93 @@ class DashboardFragment : Fragment() {
         btnAdd.setOnClickListener {
             var days = ArrayList<String>()
 
-            if (chkMonday.isChecked) days.add("Monday")
-            if (chkTuesday.isChecked) days.add("Tuesday")
-            if (chkWednesday.isChecked) days.add("Wednesday")
-            if (chkThursday.isChecked) days.add("Thursday")
-            if (chkFriday.isChecked) days.add("Friday")
-            if (chkSaturday.isChecked) days.add("Saturday")
-            if (chkSunday.isChecked) days.add("Sunday")
+            var daysBool = ArrayList<Boolean>()
+
+            if (chkMonday.isChecked) {
+                days.add("Monday")
+                daysBool.add(true)
+            }else {
+                daysBool.add(false)
+            }
+            if (chkTuesday.isChecked) {
+                days.add("Tuesday")
+                daysBool.add(true)
+            }else {
+                daysBool.add(false)
+            }
+            if (chkWednesday.isChecked) {
+                days.add("Wednesday")
+                daysBool.add(true)
+            }else {
+                daysBool.add(false)
+            }
+            if (chkThursday.isChecked) {
+                days.add("Thursday")
+                daysBool.add(true)
+            }else {
+                daysBool.add(false)
+            }
+            if (chkFriday.isChecked) {
+                days.add("Friday")
+                daysBool.add(true)
+            }else {
+                daysBool.add(false)
+            }
+            if (chkSaturday.isChecked) {
+                days.add("Saturday")
+                daysBool.add(true)
+            }else {
+                daysBool.add(false)
+            }
+            if (chkSunday.isChecked) {
+                days.add("Sunday")
+                daysBool.add(true)
+            }else {
+                daysBool.add(false)
+            }
 
             val reminder = Reminder(txtTitle.text.toString(), days, txtTime.text.toString())
 
-            HomeFragment.remindersList.add(reminder)
+            submitToDB(reminder.title, daysBool[0], daysBool[1], daysBool[2], daysBool[3], daysBool[4], daysBool[5], daysBool[6], reminder.time)
 
-            Toast.makeText(root.context, "Reminder added", Toast.LENGTH_SHORT).show()
+            HomeFragment.remindersList.add(reminder)
         }
 
 
         return root
     }
 
+    /**
+     * Crea un nuevo registro en la base de datos de firestore, la coleccion se llama actividades y tiene los siguientes campos:
+     * Actividad: string, lu, ma, mi, ju, vi, sa, do: boolean, tiempo: string
+     */
+    fun submitToDB(actividad: String, lu: Boolean, ma: Boolean, mi: Boolean, ju: Boolean, vi: Boolean, sa: Boolean, dom: Boolean, tiempo: String) {
+        val newReminder = hashMapOf(
+            "actividad" to actividad,
+            "lu" to lu,
+            "ma" to ma,
+            "mi" to mi,
+            "ju" to ju,
+            "vi" to vi,
+            "sa" to sa,
+            "do" to dom,
+            "tiempo" to tiempo
+        )
+        db.collection("actividades")
+            .add(newReminder)
+            .addOnSuccessListener {
+                Toast.makeText(this.context, "Reminder added", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this.context, "Error, try again", Toast.LENGTH_SHORT).show()
+            }
+
+
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
